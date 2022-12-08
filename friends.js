dcae7a0da8727078aab06b5484b1cfb05441f440
Elise Jones <02elise.jones02@gmail.com>
2022-12-07 18:58:57 -0700

    Add if and else if examples

---
+++ friends.js
@@ -0,0 +1,11 @@
+var bestFriend = "Maggi"
+var goodFriend = "Sull"
+if (bestFriend === "Maggie") {
+    console.log("Hello Maggie!");
+} 
+else if (goodFriend === "Sully"){
+    console.log("Hello Sully!")
+}
+else {
+    console.log("Hello Friend!");
+}
